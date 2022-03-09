51ed4ffe7681f4fe8af7c03dda7b91efadd81e1c
Caio Pereira <caioefg@gmail.com>
2022-03-09 13:08:17 -0300

    js 04-02

--- fundamentos/bloco4-02-javascript/dia2/script.js
+++ fundamentos/bloco4-02-javascript/dia2/script.js
@@ -1,4 +1,3 @@
-
 /* exercicio 04-02 ---------parte  1 e 2 ------- */
 /* 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -14,18 +13,18 @@ for( let i = 0; i < numbers.length; i++){
 
 /*  parte 3 ----media do array------ */
 
-    /* if(soma > 0) {
-        media = soma/numbers.length
-    }
-    console.log(media) */
+/* if(soma > 0) {
+    media = soma/numbers.length
+}
+console.log(media) */
 
-    /*  parte 4 ---  */
+/*  parte 4 ---  */
 
-    /* if (media > 20) {
-        console.log('valor maior que 20')
-    } else {
-        console.log('valor menor ou igual a 20')
-    } */
+/* if (media > 20) {
+    console.log('valor maior que 20')
+} else {
+    console.log('valor menor ou igual a 20')
+} */
 /* parte 5 --Utilizando for , descubra qual o maior valor contido no array e imprima-o;-- */
 
 /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
@@ -69,13 +68,75 @@ console.log(menorNumero); */
 
 /* let numeros = []
 
-    for(let i = 0; i <= 25; i++) {
+    for(let i = 1; i <= 25; i++) {
         numeros.push(i)
     }
     console.log(numeros) */
 
 /* parte 9 ---- Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2  */
 
-   /*  for (let i = 0; i < numeros.length; i++) {
-        console.log(numeros[i] /2);
-    } */
+/*  for (let i = 0; i < numeros.length; i++) {
+     console.log(numeros[i] /2);
+ } */
+
+/*  bonuss -------------------------- */
+
+/*  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+     numbers.sort((a, b) => a - b)
+
+     console.log(numbers) */
+
+/* bonus 1 modo professor */
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (numbers[index] < numbers[secondIndex]) {
+            let position = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = position;
+        }
+    }
+}
+
+console.log(numbers); */
+
+/* bonus 02 -Ordene o array numbers em ordem decrescente e imprima seus valores; */
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+     numbers.sort((a, b) => b - a)
+
+     console.log(numbers) */
+
+     /* modo professo bonus 2 */
+/* 
+     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+    for (let index = 1; index < numbers.length; index += 1) {
+        for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+            if (numbers[index] > numbers[secondIndex]) {
+             let position = numbers[index];
+             numbers[index] = numbers[secondIndex];
+             numbers[secondIndex] = position;
+    }
+  }
+}
+console.log(numbers); */
+
+/* bonus 3 ---Agora você irá criar um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (valor correspondente) e 9 (próximo valor). Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push .----- */
+
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newArray = [];
+
+for(let i = 0; i < numbers.length; i += 1){
+    if (i + 1 < numbers.length) {
+        newArray.push(numbers[i] * numbers[i + 1]);
+      } else {
+        newArray.push(numbers[i] * 2);
+      }
+    }
+    
+    console.log(newArray); */
